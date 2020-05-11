a14411a0e468036acbc3bd1d1935a37f4fca5228
Bohan-Hu <hubohancser@gmail.com>
2020-05-11 10:24:26 +0800

    update the test set

--- test.c
+++ test.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char **argv)
 {
-    Buffer buf; /* A buffer */
+    Buffer buf;         /* A buffer */
     unsigned char *blk; /* A pointer to a block */
     int i = 0;
 
@@ -43,32 +43,30 @@ int main(int argc, char **argv)
 
     char str[5];
     printf("block 1:\n");
-    for (i = 0; i < 7; i++) 
+    for (i = 0; i < 7; i++)
     {
 
         for (int k = 0; k < 4; k++)
         {
-            str[k] = *(blk + i*8 + k);
+            str[k] = *(blk + i * 8 + k);
         }
         X = atoi(str);
         for (int k = 0; k < 4; k++)
         {
-            str[k] = *(blk + i*8 + 4 + k);
+            str[k] = *(blk + i * 8 + 4 + k);
         }
         Y = atoi(str);
         printf("(%d, %d) ", X, Y);
     }
     for (int k = 0; k < 4; k++)
     {
-        str[k] = *(blk + i*8 + k);
+        str[k] = *(blk + i * 8 + k);
     }
     addr = atoi(str);
     printf("\nnext address = %d \n", addr);
-
 
     printf("\n");
     printf("IO's is %d\n", buf.numIO); /* Check the number of IO's */
 
     return 0;
 }
-
